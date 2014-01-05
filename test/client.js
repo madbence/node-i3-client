8ac20b422c5e6f169ba73bb3cb13de884dfdad29
Dányi Bence <madbence@gmail.com>
2014-01-05 21:50:33 +0100

    Test i3 query

--- test/client.js
+++ test/client.js
@@ -1,8 +1,30 @@
 var i3c = require('../');
 var should = require('should');
+var override = require('mock-fun').override;
+var restore = require('mock-fun').restore;
 
 describe('createClient', function() {
   it('should return an I3Client', function() {
     i3c.createClient().should.be.an.instanceOf(i3c.I3Client);
+  });
+});
+
+describe('Client', function() {
+  describe('#getSocketPath', function() {
+    var client, execFile;
+    var exec = function() { execFile.apply(this, arguments); };
+    beforeEach(function() {
+      client = i3c.createClient({ execFile: exec });
+      override(client, 'execFile', exec)();
+    });
+    afterEach(function() {
+      restore(client, 'execFile')();
+    });
+    it('should query name from i3', function() {
+      execFile = function(a, b, c) { c(null, ''); };
+      client.getSocketPath(function(err, cb) {
+        client.execFile.called.should.be.true;
+      });
+    });
   });
 });
