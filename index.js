44737b992e387b5f3c556ace309be88f367d59e4
Dányi Bence <madbence@gmail.com>
2014-01-05 21:52:04 +0100

    GetSocketPath initial implementation

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ exports.createClient = function createClient() {
   return new Client();
 };
 
-function Client() { }
+function Client(opts) {
+  opts = opts || {};
+  this.execFile = opts.execFile || require('child_process').execFile;
+  this.net = opts.net || require('net');
+}
+
+Client.prototype.getSocketPath = function getSocketPath(cb) {
+  this.execFile('i3', ['--get-socketpath'], function(err, out) {
+    return cb(null, this.socketPath);
+  });
+};
 
 exports.I3Client = Client;
